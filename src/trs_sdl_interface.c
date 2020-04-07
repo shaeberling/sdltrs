ccd2c221c71b61891fea19a412221c496597b9ca
Jens Guenther <dbotw@gmx.net>
2020-04-07 18:46:07 +0200

    Prevent lost keys in paste function
    ... by temporarily increasing cycles/secs ...

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -186,6 +186,7 @@ static int selectionEndX = 0;
 static int selectionEndY = 0;
 static int requestSelectAll = FALSE;
 static int timer_saved;
+static unsigned int cycles_saved;
 
 /* Support for Micro Labs Grafyx Solution and Radio Shack hi-res card */
 
@@ -1855,6 +1856,7 @@ void trs_get_event(int wait)
           paste_state = PASTE_IDLE;
           if (turbo_paste)
             timer_overclock = timer_saved;
+          cycles_per_timer = cycles_saved;
         }
         else
           paste_state = PASTE_GETNEXT;
@@ -1970,6 +1972,8 @@ void trs_get_event(int wait)
                 timer_saved = timer_overclock;
                 timer_overclock = 1;
               }
+              cycles_saved = cycles_per_timer;
+              cycles_per_timer *= 4;
               PasteManagerStartPaste();
               paste_state = PASTE_GETNEXT;
               break;
