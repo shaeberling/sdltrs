099d16d1fe798bf4ba43188407431767e0ff4b1c
Jens Guenther <dbotw@gmx.net>
2020-05-21 20:00:17 +0200

    Remove unneeded check for ROM size

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -1178,8 +1178,7 @@ void trs_rom_init(void)
         if (trs_load_rom(romfile) == 0)
           break;
       }
-      if (trs_rom1_size > 0)
-        trs_load_compiled_rom(trs_rom1_size, trs_rom1);
+      trs_load_compiled_rom(trs_rom1_size, trs_rom1);
       break;
     case 3:
     case 4:
@@ -1187,16 +1186,14 @@ void trs_rom_init(void)
         if (trs_load_rom(romfile3) == 0)
           break;
       }
-      if (trs_rom3_size > 0)
-        trs_load_compiled_rom(trs_rom3_size, trs_rom3);
+      trs_load_compiled_rom(trs_rom3_size, trs_rom3);
       break;
     case 5:
       if (romfile4p[0]) {
         if (trs_load_rom(romfile4p) == 0)
           break;
       }
-      if (trs_rom4p_size > 0)
-        trs_load_compiled_rom(trs_rom4p_size, trs_rom4p);
+      trs_load_compiled_rom(trs_rom4p_size, trs_rom4p);
       break;
   }
 
