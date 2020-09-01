3c49c08e2ab2e2825d65f0f5745c0a1dbf2097bd
Jens Guenther <dbotw@gmx.net>
2020-09-01 19:39:30 +0200

    Ensure unsigned screen position
    ... this also slightly optimizes bit shift operations ...

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -2625,12 +2625,13 @@ void trs_turbo_led(void)
   drawnRectCount = MAX_RECTS;
 }
 
-void trs_screen_write_char(int position, unsigned char char_index)
+void trs_screen_write_char(unsigned int position, unsigned char char_index)
 {
-  int row, col, expanded;
+  unsigned int row, col;
+  int expanded;
   SDL_Rect srcRect, dstRect;
 
-  if (position >= screen_chars)
+  if (position >= (unsigned int)screen_chars)
     return;
   trs_screen[position] = char_index;
   if ((currentmode & EXPANDED) && (position & 1))
