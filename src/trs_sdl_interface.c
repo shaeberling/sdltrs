a3f780af7f8f014f20c0843720103e70d9bc39d0
Jens Guenther <dbotw@gmx.net>
2020-09-03 20:10:06 +0200

    Fix old-style function definition

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -1595,7 +1595,7 @@ static void trs_flip_fullscreen(void)
   SDL_SetWindowFullscreen(window, fullscreen ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0);
 }
 
-static char *trs_get_copy_data()
+static char *trs_get_copy_data(void)
 {
   static char copy_data[2048];
   char *curr_data = copy_data;
