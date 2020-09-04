d0465af644a6a4a8c40ac267e269010122d39d59
Jens Guenther <dbotw@gmx.net>
2020-09-04 18:01:08 +0200

    Set empty SDL2 window title

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -84,7 +84,6 @@
 #define INVERSE   2
 #define ALTERNATE 4
 
-extern char *program_name;
 extern char trs_char_data[][MAXCHARS][TRS_CHAR_HEIGHT];
 extern int  key_queue[KEY_QUEUE_SIZE];
 extern int  key_queue_head;
@@ -1255,7 +1254,7 @@ void trs_screen_init(void)
   screen_height = OrigHeight - led_height;
 
   if (window == NULL) {
-    window = SDL_CreateWindow(program_name,
+    window = SDL_CreateWindow(NULL,
                               SDL_WINDOWPOS_CENTERED,
                               SDL_WINDOWPOS_CENTERED,
                               800, 600,
