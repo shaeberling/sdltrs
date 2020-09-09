a67c12ca63021fde6ed072d4098a25bef8b123d9
Jens Guenther <dbotw@gmx.net>
2020-09-09 20:26:16 +0200

    Simplify Draw Selection Rectangle

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -1339,11 +1339,9 @@ static void DrawSelectionRectangle(int orig_x, int orig_y, int copy_x, int copy_
 {
   int const bpp   = screen->format->BytesPerPixel;
   int const pitch = screen->pitch;
-  int const width = scale * pitch;
   Uint8 *pixels   = screen->pixels;
   Uint8 *pixel;
-  int x, y, z;
-  int dist_x;
+  int x, y;
 
   if (copy_x < orig_x) {
     int swap_x;
@@ -1362,36 +1360,33 @@ static void DrawSelectionRectangle(int orig_x, int orig_y, int copy_x, int copy_
 
   SDL_LockSurface(screen);
 
-  dist_x  = orig_x * bpp;
+  orig_x *= bpp;
+  copy_x *= bpp;
   copy_y *= pitch;
   orig_y *= pitch;
-  for (y = orig_y; y < orig_y + width; y += pitch) {
-    pixel = pixels + y + dist_x;
-    for (x = 0; x < copy_x - orig_x + scale; x++)
-      for (z = 0; z < bpp; z++)
-        *pixel++ ^= 0xFF;
+
+  for (y = orig_y; y < orig_y + pitch; y += pitch) {
+    pixel = pixels + y + orig_x;
+    for (x = 0; x < copy_x - orig_x + bpp; x++)
+      *pixel++ ^= 0xFF;
   }
   if (copy_y > orig_y) {
-    for (y = copy_y; y < copy_y + width; y += pitch) {
-      pixel = pixels + y + dist_x;
-      for (x = 0; x < copy_x - orig_x + scale; x++)
-        for (z = 0; z < bpp; z++)
-          *pixel++ ^= 0xFF;
+    for (y = copy_y; y < copy_y + pitch; y += pitch) {
+      pixel = pixels + y + orig_x;
+      for (x = 0; x < copy_x - orig_x + bpp; x++)
+        *pixel++ ^= 0xFF;
     }
   }
-  for (y = orig_y + width; y < copy_y; y += pitch) {
-    pixel = pixels + y + dist_x;
-    for (x = 0; x < scale; x++)
-      for (z = 0; z < bpp; z++)
-        *pixel++ ^= 0xFF;
+  for (y = orig_y + pitch; y < copy_y; y += pitch) {
+    pixel = pixels + y + orig_x;
+    for (x = 0; x < bpp; x++)
+      *pixel++ ^= 0xFF;
   }
   if (copy_x > orig_x) {
-    dist_x = copy_x * bpp;
-    for (y = orig_y + width; y < copy_y; y += pitch) {
-      pixel = pixels + y + dist_x;
-        for (x = 0; x < scale; x++)
-          for (z = 0; z < bpp; z++)
-            *pixel++ ^= 0xFF;
+    for (y = orig_y + pitch; y < copy_y; y += pitch) {
+      pixel = pixels + y + copy_x;
+        for (x = 0; x < bpp; x++)
+          *pixel++ ^= 0xFF;
     }
   }
 
