ca025df857c05cf57838b284c80474b26e52f7e2
Jens Guenther <dbotw@gmx.net>
2020-09-13 20:21:38 +0200

    Simplify GUI question dialog

--- src/trs_sdl_gui.c
+++ src/trs_sdl_gui.c
@@ -1167,12 +1167,7 @@ int trs_gui_display_popup_matrix(const char* title, const char **entry,
 
 int trs_gui_display_question(const char *text)
 {
-  const char *answer_choices[] = {
-    "       No        ",
-    "       Yes       "
-  };
-
-  return trs_gui_display_popup(text, answer_choices, 2, 0);
+  return trs_gui_display_popup(text, yes_no_choices, 2, 0);
 }
 
 int trs_gui_file_overwrite(void)
@@ -1180,7 +1175,7 @@ int trs_gui_file_overwrite(void)
   struct stat st;
 
   if (stat(filename, &st) == 0 && S_ISREG(st.st_mode))
-    return trs_gui_display_question("Overwrite file?");
+    return trs_gui_display_question("Overwrite?");
 
   return 1;
 }
@@ -2662,7 +2657,7 @@ void trs_gui_exec_cmd(void)
 
 int trs_gui_exit_sdltrs(void)
 {
-  return trs_gui_display_question("Quit SDLTRS?");
+  return trs_gui_display_question("Quit?");
 }
 
 void trs_gui_save_bmp(void)
