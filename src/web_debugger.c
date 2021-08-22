e147dda3b9109d44d210bf920686ccee6ae3bf4f
Sascha Häberling <mail@sascha-haeberling.com>
2021-08-22 15:39:28 -0700

    Add basic frontend control, also clean-up data structures.

--- src/web_debugger.c
+++ src/web_debugger.c
@@ -12,14 +12,31 @@
 
 static struct mg_mgr www_mgr;
 static SDL_Thread* thread;
-struct mg_connection* status_conn = NULL;
+static struct mg_connection* status_conn = NULL;
+static TRX_Context* ctx = NULL;
 
 static bool init_webserver(void);
 static char* get_registers_json(void);
 
 // public
-bool init_web_debugger(void) {
-	return init_webserver();
+bool init_trs_xray(TRX_Context* ctx_param) {
+	if (!init_webserver()) {
+    puts("ERROR(TRX): Aborting initialization.");
+    return false;
+  }
+  ctx = ctx_param;
+  return true; // TODO
+}
+
+// public
+void send_update_to_web_debugger() {
+  if (status_conn == NULL) {
+    return;
+  }
+  // Send registers.
+  char* message = get_registers_json();
+  mg_ws_send(status_conn, message, strlen(message), WEBSOCKET_OP_TEXT);
+  free(message);
 }
 
 static void handle_http_request(struct mg_connection *conn,
@@ -30,16 +47,12 @@ static void handle_http_request(struct mg_connection *conn,
     mg_http_reply(conn, 200, "Content-Type: application/javascript\r\nConnection: close\r\n", web_debugger_js);
   } else if (mg_http_match_uri(message, "/web_debugger.css")) {
     mg_http_reply(conn, 200, "Content-Type: text/css\r\nConnection: close\r\n", web_debugger_css);
-  } else if (mg_http_match_uri(message, "/registers")) {
+  } else if (mg_http_match_uri(message, "/channel")) {
 		mg_ws_upgrade(conn, message, NULL);
 		status_conn = conn;
   } else if (mg_http_match_uri(message, "/action")) {
     mg_http_reply(conn, 200, "Content-Type: text/plain\r\nConnection: close\r\n", "ACTION!");
-		if (status_conn != NULL) {
-			char* message = get_registers_json();
-			mg_ws_send(status_conn, message, strlen(message), WEBSOCKET_OP_TEXT);
-			free(message);
-		}
+    send_update_to_web_debugger();
   } else {
     // Resource not found.
     mg_http_reply(conn, 404, "Content-Type: text/html\r\nConnection: close\r\n", "");
@@ -48,45 +61,70 @@ static void handle_http_request(struct mg_connection *conn,
 
 static char* get_registers_json(void) {
 	  cJSON* json = cJSON_CreateObject();
-    cJSON_AddNumberToObject(json, "a", Z80_A);
-    cJSON_AddNumberToObject(json, "b", Z80_B);
-    cJSON_AddNumberToObject(json, "c", Z80_C);
-    cJSON_AddNumberToObject(json, "d", Z80_D);
-    cJSON_AddNumberToObject(json, "e", Z80_E);
-    cJSON_AddNumberToObject(json, "f", Z80_F);
-    cJSON_AddNumberToObject(json, "h", Z80_H);
-    cJSON_AddNumberToObject(json, "l", Z80_L);
-    cJSON_AddNumberToObject(json, "ix", Z80_IX);
-    cJSON_AddNumberToObject(json, "iy", Z80_IY);
-    cJSON_AddNumberToObject(json, "pc", Z80_PC);
-    cJSON_AddNumberToObject(json, "sp", Z80_SP);
-    cJSON_AddNumberToObject(json, "af", Z80_AF_PRIME);
-    cJSON_AddNumberToObject(json, "bc", Z80_BC_PRIME);
-    cJSON_AddNumberToObject(json, "de", Z80_DE_PRIME);
-    cJSON_AddNumberToObject(json, "hl", Z80_HL_PRIME);
-    cJSON_AddNumberToObject(json, "i", Z80_I);
-    cJSON_AddNumberToObject(json, "r_1", Z80_R7);
-    cJSON_AddNumberToObject(json, "r_2", (Z80_R & 0x7f));
 
-    cJSON_AddNumberToObject(json, "z80_t_state_counter", z80_state.t_count);
-    cJSON_AddNumberToObject(json, "z80_clockspeed", z80_state.clockMHz);
-		cJSON_AddNumberToObject(json, "z80_iff1", z80_state.iff1);
-    cJSON_AddNumberToObject(json, "z80_iff2", z80_state.iff2);
-    cJSON_AddNumberToObject(json, "z80_interrupt_mode", z80_state.interrupt_mode);
+    cJSON* context = cJSON_CreateObject();
+    cJSON_AddStringToObject(context, "system_name", ctx->system_name);
+    cJSON_AddNumberToObject(context, "model", ctx->model);
+    cJSON_AddItemToObject(json, "context", context);
 
-    cJSON_AddNumberToObject(json, "flag_sign", SIGN_FLAG != 0);
-    cJSON_AddNumberToObject(json, "flag_zero", ZERO_FLAG != 0);
-    cJSON_AddNumberToObject(json, "flag_undoc5", (Z80_F & UNDOC5_MASK) != 0);
-    cJSON_AddNumberToObject(json, "flag_half_carry", HALF_CARRY_FLAG != 0);
-    cJSON_AddNumberToObject(json, "flag_undoc3", (Z80_F & UNDOC3_MASK) != 0);
-    cJSON_AddNumberToObject(json, "flag_overflow", OVERFLOW_FLAG != 0);
-    cJSON_AddNumberToObject(json, "flag_subtract", SUBTRACT_FLAG != 0);
-    cJSON_AddNumberToObject(json, "flag_carry", CARRY_FLAG != 0);
+    cJSON* registers = cJSON_CreateObject();
+    cJSON_AddNumberToObject(registers, "pc", Z80_PC);
+    cJSON_AddNumberToObject(registers, "sp", Z80_SP);
+    cJSON_AddNumberToObject(registers, "af", Z80_AF);
+    cJSON_AddNumberToObject(registers, "bc", Z80_BC);
+    cJSON_AddNumberToObject(registers, "de", Z80_DE);
+    cJSON_AddNumberToObject(registers, "hl", Z80_HL);
+    cJSON_AddNumberToObject(registers, "af_prime", Z80_AF_PRIME);
+    cJSON_AddNumberToObject(registers, "bc_prime", Z80_BC_PRIME);
+    cJSON_AddNumberToObject(registers, "de_prime", Z80_DE_PRIME);
+    cJSON_AddNumberToObject(registers, "hl_prime", Z80_HL_PRIME);
+    cJSON_AddNumberToObject(registers, "ix", Z80_IX);
+    cJSON_AddNumberToObject(registers, "iy", Z80_IY);
+    cJSON_AddNumberToObject(registers, "i", Z80_I);
+    cJSON_AddNumberToObject(registers, "r_1", Z80_R7);
+    cJSON_AddNumberToObject(registers, "r_2", (Z80_R & 0x7f));
+    
+    cJSON_AddNumberToObject(registers, "z80_t_state_counter", z80_state.t_count);
+    cJSON_AddNumberToObject(registers, "z80_clockspeed", z80_state.clockMHz);
+		cJSON_AddNumberToObject(registers, "z80_iff1", z80_state.iff1);
+    cJSON_AddNumberToObject(registers, "z80_iff2", z80_state.iff2);
+    cJSON_AddNumberToObject(registers, "z80_interrupt_mode", z80_state.interrupt_mode);
+
+    cJSON_AddNumberToObject(registers, "flag_sign", SIGN_FLAG != 0);
+    cJSON_AddNumberToObject(registers, "flag_zero", ZERO_FLAG != 0);
+    cJSON_AddNumberToObject(registers, "flag_undoc5", (Z80_F & UNDOC5_MASK) != 0);
+    cJSON_AddNumberToObject(registers, "flag_half_carry", HALF_CARRY_FLAG != 0);
+    cJSON_AddNumberToObject(registers, "flag_undoc3", (Z80_F & UNDOC3_MASK) != 0);
+    cJSON_AddNumberToObject(registers, "flag_overflow", OVERFLOW_FLAG != 0);
+    cJSON_AddNumberToObject(registers, "flag_subtract", SUBTRACT_FLAG != 0);
+    cJSON_AddNumberToObject(registers, "flag_carry", CARRY_FLAG != 0);
+    cJSON_AddItemToObject(json, "registers", registers);
+
     char* str = cJSON_PrintUnformatted(json);
     cJSON_Delete(json);
     return str;
 }
 
+static void on_frontend_message(const char* msg) {
+  if (strcmp(msg, "action/refresh") == 0) {
+    send_update_to_web_debugger();
+  } else if (strcmp(msg, "action/step") == 0) {
+    ctx->control_callback(TRX_CONTROL_TYPE_STEP);
+  } else if (strcmp(msg, "action/step-over") == 0) {
+    ctx->control_callback(TRX_CONTROL_TYPE_STEP_OVER);
+  } else if (strcmp(msg, "action/continue") == 0) {
+    ctx->control_callback(TRX_CONTROL_TYPE_CONTINUE);
+  } else if (strcmp(msg, "action/pause") == 0) {
+    ctx->control_callback(TRX_CONTROL_TYPE_PAUSE);
+  } else if (strcmp(msg, "action/soft_reset") == 0) {
+    ctx->control_callback(TRX_CONTROL_TYPE_SOFT_RESET);
+  } else if (strcmp(msg, "action/hard_reset") == 0) {
+    ctx->control_callback(TRX_CONTROL_TYPE_HARD_RESET);
+  } else {
+    printf("WARNING(TRX): Unknown message: '%s'", msg);
+  }
+  send_update_to_web_debugger();
+}
 
 static void www_handler(struct mg_connection *conn,
                         int ev, void *ev_data, void *fn_data) {
@@ -95,6 +133,14 @@ static void www_handler(struct mg_connection *conn,
 	  handle_http_request(conn, (struct mg_http_message*) ev_data);
 		break;
 	}
+  case MG_EV_WS_MSG: {
+    static char message[50];
+    struct mg_ws_message *wm = (struct mg_ws_message *) ev_data;
+    strncpy(message, wm->data.ptr, wm->data.len);
+    message[wm->data.len] = '\0';
+    on_frontend_message(message);
+    break;
+  }
 	case MG_EV_CLOSE: {
 		if (conn == status_conn) {
 			status_conn = NULL;
--- src/web_debugger.h
+++ src/web_debugger.h
@@ -1,5 +1,124 @@
-
 #include <stdbool.h>
+#include <stdint.h>
+
+typedef struct {
+  uint16_t af;
+  uint16_t bc;
+  uint16_t de;
+  uint16_t hl;
+  uint16_t af_prime;
+  uint16_t bc_prime;
+  uint16_t de_prime;
+  uint16_t hl_prime;
+
+  uint16_t pc;
+  uint16_t sp;
+  uint16_t ix;
+  uint16_t iy;
+
+  uint16_t i;
+  uint16_t iff1;
+  uint16_t iff2;
+} TRX_StatusRegistersAndFlags;
+
+typedef enum {
+  TRX_SCREEN_NORMAL = 0,
+  TRX_SCREEN_EXPANDED = 1,
+  TRX_SCREEN_ALTERNATE = 2,
+  TRX_SCREEN_INVERTED = 3
+} TRX_ScreenMode;
+
+typedef struct {
+  bool paused;
+  TRX_StatusRegistersAndFlags registers;
+  TRX_ScreenMode screen_mode;
+} TRX_SystemState;
+
+typedef enum {
+  UNDEFINED = 0,
+  MODEL_I = 1,
+  MODEL_II = 2,
+  MODEL_III = 3,
+  MODEL_IV = 4,
+  MODEL_IV_P = 5,
+} TRX_ModelType;
+
+typedef struct {
+  uint32_t start;
+  uint32_t length;
+} TRX_MemoryRange;
+
+typedef struct {
+  bool control_step;
+  bool control_step_over;
+  bool control_continue;
+  bool control_pause;
+  bool pc_breakpoints;
+  bool memory_breakpoints;
+  bool io_breakpoints;
+  TRX_MemoryRange memory_range;
+  // TOOD: Supported Memory Segment. (A single segment should suffice)
+  //       Model 1 demo will only work on address 32k and up.
+  //       (For read and write)
+} TRX_Capabilities;
+
+typedef void (*TRX_UpdateState)(const TRX_SystemState* state);
+
+typedef enum {
+  TRX_CONTROL_TYPE_STEP = 0,
+  TRX_CONTROL_TYPE_STEP_OVER = 1,
+  TRX_CONTROL_TYPE_CONTINUE = 2,
+  TRX_CONTROL_TYPE_PAUSE = 3,
+  TRX_CONTROL_TYPE_SOFT_RESET = 4,
+  TRX_CONTROL_TYPE_HARD_RESET = 5
+} TRX_CONTROL_TYPE;
+typedef void (*TRX_ControlCallback)(TRX_CONTROL_TYPE type);
+
+typedef enum {
+  TRX_BREAK_PC = 0,
+  TRX_BREAK_MEMORY = 1,
+  TRX_BREAK_IO = 2
+} TRX_BREAK_TYPE;
+typedef void (*TRX_SetBreakPointCallback)(int bp_id, uint8_t addr, TRX_BREAK_TYPE type);
+typedef void (*TRX_RemoveBreakPointCallback)(int bp_id);
+
+typedef struct {
+  TRX_MemoryRange range;
+  uint8_t* data;
+} TRX_MemorySegment;
+typedef void (*TRX_GetMemorySegment)(int start, int length, TRX_MemorySegment* segment);
+typedef void (*TRX_SetMemorySegment)(TRX_MemorySegment* segment);
+
+typedef struct {
+  // Descriptive name of the system under test (SUT).
+  char* system_name;
+
+  // TRS model type.
+  TRX_ModelType model;
+
+  // Original = 0 or 1.
+  uint8_t rom_version;
+
+  // Lets the frontend know about the SUT's capabilities. 
+  TRX_Capabilities capabilities;
+
+  // Callbacks invoked from the frontend.
+  TRX_ControlCallback control_callback;
+  TRX_SetBreakPointCallback breakpoint_callback;
+  TRX_RemoveBreakPointCallback remove_breakpoint_callback;
+  TRX_GetMemorySegment get_memory_segment;
+  TRX_SetMemorySegment set_memory_segment;
+
+  // Tells the frontend about state changes of the SUT.
+  const TRX_UpdateState update_state;
+
+} TRX_Context;
 
 // Initialize the web debugger.
-bool init_web_debugger(void);
+bool init_trs_xray(TRX_Context* ctx);
+
+// Questions:
+// - When are the different opcodes being used? http://z80-heaven.wikidot.com/opcode-reference-chart
+//   - minor: CB, CC, ED, FD, DDCB, FDCB etc.
+// - To know how to get N lines of code, one would need to know the width of the opcodes/arguments.
+//  - Could just ask for memory blocks
